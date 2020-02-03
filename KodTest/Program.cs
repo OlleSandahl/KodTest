4e721d5c2945102dad7f2ff39e33cddb9a690840
olle sandahl <olle.sandahl@live.se>
2020-02-03 11:24:50 +0100

    Add project files.

---
+++ KodTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KodTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ditt namn");
+            string namn = Console.ReadLine();
+
+            Console.Clear();
+           
+            Console.WriteLine("Hej " + namn);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
